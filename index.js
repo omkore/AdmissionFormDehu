973d0f0e00f1b85a8b826ac987d5c4a1985ce2f3
0 |\/| <70907516+omkore@users.noreply.github.com>
2025-01-04 11:28:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+let popup = document.getElementById("show")
+
+
+function show(){
+    popup.classList.remove("hide")
+    popup.classList.add("show")
+}
+
+function hide(){
+    popup.classList.remove("show")
+    popup.classList.add("hide")
+}
